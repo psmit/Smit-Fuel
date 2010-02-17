455737fe6ca8fed98f3279f2d47ab8af57ddc4d7
Peter Smit <peter@smitmail.eu>
2010-02-17 13:56:03 +0200

    Version 2 ready, working to upload refuellings

--- fuel.py
+++ fuel.py
@@ -9,7 +9,7 @@ from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
 class Refueling(db.Model):
-	author = db.UserProperty()
+	user = db.UserProperty()
 	date = db.DateTimeProperty(auto_now_add=True)
 	liters = db.FloatProperty()
 	liter_price = db.IntegerProperty()
@@ -19,22 +19,45 @@ class Refueling(db.Model):
 
 class FormPage(webapp.RequestHandler):
 	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'form.html')
-		self.response.out.write(template.render(path, { }))
+		if users.get_current_user():
+			path = os.path.join(os.path.dirname(__file__), 'form.html')
+			self.response.out.write(template.render(path, { }))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
 
 class SubmitPage(webapp.RequestHandler):
 	def post(self):	
-		path = os.path.join(os.path.dirname(__file__), 'form.html')
+		if users.get_current_user():
+			refueling = Refueling()
+			
+			refueling.user = users.get_current_user()
+			refueling.liters = float(self.request.get('liters'))
+			refueling.liter_price = int(round(float(self.request.get('literprice')), 3)* 1000)
+			refueling.total_price = int(round((refueling.liter_price * refueling.liters) / 10, 0))
+			refueling.odo = int(self.request.get('odo'))
+			
+			refueling.put()
+			self.redirect('/list')
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 		
-		
-class ConfirmPage(webapp.RequestHandler):
+class ListPage(webapp.RequestHandler):
 	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'form.html')
+		refueling_query = Refueling.all().order('-date')
+		refuelings = refueling_query.fetch(10)
 		
+		template_values = {
+			'refuelings': refuelings,
+		}
+		
+		path = os.path.join(os.path.dirname(__file__), 'list.html')
+		self.response.out.write(template.render(path, template_values))
 	
 application = webapp.WSGIApplication(
                                      [('/', FormPage),
-                                      ('/create', SubmitPage)],
+                                      ('/create', SubmitPage),
+                                      ('/list', ListPage)],
                                      debug=True)
 
 def main():
