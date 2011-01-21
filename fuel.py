55438d95b8c269b7c8854790971cea08df7fd9a3
Peter Smit <peter@cis.hut.fi>
2011-01-21 19:07:52 +0200

    Introduced some fuel algorithms

--- fuel.py
+++ fuel.py
@@ -9,65 +9,84 @@ from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
 class Refueling(db.Model):
-	user = db.UserProperty()
-	date = db.DateTimeProperty(auto_now_add=True)
-	liters = db.FloatProperty()
-	liter_price = db.IntegerProperty()
-	total_price = db.IntegerProperty()
-	odo = db.IntegerProperty()
-	
-	def real_liter_price(self):
-		return float(self.liter_price) / 1000.0
-		
-	def real_total_price(self):
-		return float(self.total_price) / 1000.0
+    user = db.UserProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+    liters = db.FloatProperty()
+    rest_liters = db.FloatProperty(default=0.0)
+    liter_price = db.IntegerProperty()
+    total_price = db.IntegerProperty()
+    odo = db.IntegerProperty()
+
+    def real_liter_price(self):
+        return float(self.liter_price) / 1000.0
+
+    def real_total_price(self):
+        return float(self.total_price) / 1000.0
+
+    def __str__(self):
+        return "%s %.2f %d" %(self.date, self.liters,self.odo)
+
+
 
 class FormPage(webapp.RequestHandler):
-	def get(self):
-		if users.get_current_user():
-			path = os.path.join(os.path.dirname(__file__), 'template/form.html')
-			self.response.out.write(template.render(path, { }))
-		else:
-			self.redirect(users.create_login_url(self.request.uri))
+    def get(self):
+        if users.get_current_user():
+            path = os.path.join(os.path.dirname(__file__), 'template/form.html')
+            self.response.out.write(template.render(path, { }))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 
 class SubmitPage(webapp.RequestHandler):
-	def post(self):	
-		if users.get_current_user():
-			refueling = Refueling()
-			
-			refueling.user = users.get_current_user()
-			refueling.liters = float(self.request.get('liters'))
-			refueling.liter_price = int(round(float(self.request.get('literprice')), 3)* 1000)
-			refueling.total_price = int(round((refueling.liter_price * refueling.liters), 0))
-			refueling.odo = int(self.request.get('odo'))
-			
-			refueling.put()
-			self.redirect('/list')
-		else:
-			self.redirect(users.create_login_url(self.request.uri))
-		
+    def post(self):
+        if users.get_current_user():
+            refueling = Refueling()
+
+            refueling.user = users.get_current_user()
+            refueling.liters = float(self.request.get('liters'))
+            refueling.liter_price = int(round(float(self.request.get('literprice')), 3)* 1000)
+            refueling.total_price = int(round((refueling.liter_price * refueling.liters), 0))
+            refueling.odo = int(self.request.get('odo'))
+
+            refueling.put()
+            self.redirect('/list')
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
 class ListPage(webapp.RequestHandler):
-	def get(self):
-		refueling_query = Refueling.all().order('-date')
-		refuelings = refueling_query.fetch(10)
-		
-		template_values = {
-			'refuelings': refuelings,
-		}
-		
-		path = os.path.join(os.path.dirname(__file__), 'template/list.html')
-		self.response.out.write(template.render(path, template_values))
-	
+    def get(self):
+        refueling_query = Refueling.all().order('-date')
+        refuelings = refueling_query.fetch(10)
+
+        template_values = {
+            'refuelings': refuelings,
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'template/list.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+class FuelPrice(webapp.RequestHandler):
+    def get(self):
+        template_values = {
+            'refuelings': Refueling.all().order('-date')
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'template/fuelprice.html')
+        self.response.out.write(template.render(path, template_values))
+
+
 application = webapp.WSGIApplication(
                                      [('/', FormPage),
                                       ('/create', SubmitPage),
-                                      ('/list', ListPage)],
+                                      ('/list', ListPage),
+                                      ('/fuelprice', FuelPrice),
+                                      ],
                                      debug=True)
 
 def main():
-	run_wsgi_app(application)
+    run_wsgi_app(application)
 
 if __name__ == "__main__":
-	main()
+    main()
 
---
+++ fuelmath.py
@@ -0,0 +1,54 @@
+
+
+def run_restliter_algo(refuelings):
+    def smooth_forward(refuelings,convergence_speed=0.8,tank_size = None):
+        if tank_size is None:
+            tank_size = max(x.liters for x in refuelings)
+
+        start_average = _get_average(refuelings,1)
+        update_low(refuelings,0,start_average,convergence_speed,tank_size)
+        for i in xrange(len(refuelings)-2):
+            update_high(refuelings,i,_get_average(refuelings,i+1),convergence_speed,tank_size)
+
+
+    def smooth_backward(refuelings,convergence_speed=0.5,tank_size = None):
+        if tank_size is None:
+            tank_size = max(x.liters for x in refuelings)
+
+        start_average = _get_average(refuelings,len(refuelings)-3)
+
+        update_high(refuelings,len(refuelings)-2,start_average,convergence_speed + (1-convergence_speed) * 0.5 ,tank_size)
+
+        for i in xrange(len(refuelings)-2,1,-1):
+            update_low(refuelings,i,_get_average(refuelings,i-1),convergence_speed + (1-convergence_speed) * 0.5,tank_size)
+
+
+
+    def update_high(refuelings,i,avg,convergence_speed,tank_size):
+        desired_rest =  (-1.0 * avg * (refuelings[i+1].odo-refuelings[i].odo))+refuelings[i].liters+refuelings[i].rest_liters
+
+        desired_rest = refuelings[i+1].rest_liters + (convergence_speed * (desired_rest-refuelings[i+1].rest_liters))
+
+        refuelings[i+1].rest_liters = _get_within_bounds(0,tank_size-refuelings[i+1].liters,desired_rest)
+
+
+
+
+    def update_low(refuelings,i,avg,convergence_speed,tank_size):
+        desired_rest = (avg * (refuelings[i+1].odo-refuelings[i].odo))-refuelings[i].liters+refuelings[i+1].rest_liters
+
+        desired_rest = refuelings[i].rest_liters + (convergence_speed * (desired_rest-refuelings[i].rest_liters))
+
+        refuelings[i].rest_liters = _get_within_bounds(0,tank_size-refuelings[i].liters,desired_rest)
+
+    def _get_average(refuelings, i):
+        return (refuelings[i].liters + refuelings[i].rest_liters - refuelings[i+1].rest_liters) / (refuelings[i+1].odo - refuelings[i].odo)
+
+
+    def _get_within_bounds(min_val, max_val, val):
+        return min(max_val,max(min_val,val))
+
+    for i in xrange(4):
+        smooth_forward(refuelings,1.0/(i+1.0))
+        smooth_backward(refuelings,1.0/(i+1.0))
+
